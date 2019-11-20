adad9f2e0731da24a47df0ee73741f7223cad38c
Gretch Gapol <gapolgretchen@gmail.com>
2019-11-21 10:37:03 +1100

    no browser

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -132,3 +132,9 @@ RUN apt-get -y install libgconf-2-4 \
   && mv chromedriver /usr/local/bin/chromedriver \
   && chmod +x /usr/local/bin/chromedriver"
 fi
+
+echo "# install yarn
+RUN apt-get update && apt-get install -y apt-transport-https \
+&& curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add - \
+&& echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list \
+&& apt-get update && apt-get install yarn"
